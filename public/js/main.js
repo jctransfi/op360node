8ba43212a91ccd35f3a1708396296429bfd3afc5
jctransfi <jctransfi@gmail.com>
2015-04-24 16:15:11 +0800

    query string
    changed query string stuff

--- public/js/main.js
+++ public/js/main.js
@@ -93,8 +93,8 @@ myApp.controller('paController', function($scope, dataService, uiGridConstants) 
 
   	$scope.update = function (cpe){
   		console.log("beep")
-  		var updateQ = {"vhid": $scope.cpe.vhid, "descr":  $scope.cpe.device + "" + $scope.cpe.desc1 + "/" + $scope.cpe.desc2 + "/" + $scope.cpe.desc3,
-  		"stdt":  $scope.cpe.stdt + " " + $scope.cpe.sttm, "endt":  $scope.cpe.enddt + " " + $scope.cpe.endtm }
+  		var updateQ = {"vhid": $scope.cpe.vhid, "descr":  $scope.cpe.device + "" + $scope.cpe.desc1 + "%2F" + $scope.cpe.desc2 + "%2F" + $scope.cpe.desc3,
+  		"stdt":  $scope.cpe.stdt + "+" + $scope.cpe.sttm, "endt":  $scope.cpe.enddt + "+" + $scope.cpe.endtm }
 
   		console.log(updateQ);
 
