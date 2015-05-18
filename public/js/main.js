0e5b83008446101c22ce7109aea4d47186216ada
jctransfi <jctransfi@gmail.com>
2015-05-18 13:34:02 +0800

    Merged IF Description to single field

--- public/js/main.js
+++ public/js/main.js
@@ -50,7 +50,7 @@ myApp.controller('contactController', function($scope) {
 myApp.controller('paController', function($scope, dataService, uiGridConstants) {
 	var defQ = {"vhid":"CABRCV1-CROCS-RTR-1", "descr":"Serial0%2F0%2F0", "stdt":"2015-01-01+00:00:00", "endt":"2015-01-01+23:59:00"};
 
-	$scope.master = {vhid:"CABRCV1-CROCS-RTR-1", device: "Serial", desc1: "0", desc2:"0", desc3: "0", stdt: "2015-01-01", sttm: "00:00:00", enddt: "2015-01-01", endtm: "01:00:00"};
+	$scope.master = {vhid:"CABRCV1-CROCS-RTR-1", device: "Serial", desc: "0/0/0", stdt: "2015-01-01", sttm: "00:00:00", enddt: "2015-01-01", endtm: "01:00:00"};
 	$scope.cpe = angular.copy($scope.master);
 
 	$scope.promise = null;
