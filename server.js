521d67f70fb4565ab841a04c2bc032fef8037012
jctransfi <jctransfi@gmail.com>
2015-05-20 13:11:35 +0800

    updating server again

--- server.js
+++ server.js
@@ -92,50 +92,6 @@ router.route('/bears/:descr/:endt/:stdt/:vhid')
     http.request(options, callback).end();
   });
 
-router.route('/nerdstats/:descr/:endt/:stdt/:vhid')
-
-  // get all the bears (accessed at GET http://localhost:8080/api/bears)
-  .get(function(req, res) {
-    var http = require('http');
-
-    var descr = req.params.descr;
-    var endt = req.params.endt;
-    var stdt = req.params.stdt;
-    var vhid = req.params.vhid;
-
-    var urlpath = '/stats/search/findByVhidAndDescrAndEnddtBetween?descr='+descr+'&endt='+endt+'&stdt='+stdt+'&vhid='+vhid;
-
-    //The url we want is: 'www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new'
-    var options = {
-      host: 'dcoeng1-paev-1.tnc.virtela.cc',
-      path: urlpath,
-      port: 8090,
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json'
-      }
-
-    };
-
-    callback = function(response) {
-      var str = '';
-
-      //another chunk of data has been recieved, so append it to `str`
-      response.on('data', function (chunk) {
-        str += chunk;
-      });
-
-      //the whole response has been recieved, so we just print it out here
-      response.on('end', function () {
-        var parsed = JSON.parse(str)
-        console.log(str);
-        res.json(parsed);
-      });
-    }
-
-    http.request(options, callback).end();
-  });
-
 app.use('/api', router);
 app.use(express.static('public'));
 
